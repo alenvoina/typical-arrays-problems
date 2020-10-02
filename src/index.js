3e5564b5bf33063b223e6dbe767a87a9d6867ce2
alenvoina <70748371+alenvoina@users.noreply.github.com>
2020-10-02 13:06:04 +0300

    feat: finish typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array = []) {
+  if (array.length === 0) return 0
 
-exports.min = function min (array) {
-  return 0;
+   let min = Infinity
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+        }
+    }
+
+  return min
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+  if (array.length === 0) return 0
+
+    let max = -Infinity
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i]
+      }
+    }
+    return max
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+  if (array.length === 0) return 0
+
+  let avg = 0
+  for (let i = 0; i < array.length; i++) {
+      avg+=array[i]
+    }
+    avg/=array.length
+  return avg
+
 }
